7b75dfafab990622fe50a382061afef17b14d6c2
Deepanshu Choudhary <deepengineeryt@gmail.com>
2026-01-25 23:59:34 +0530

    Working on HomeUI panel

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -6,27 +6,27 @@ const LocationSearchPanel = () => {
 
             {/* This is just a Sample Data */}
 
-            <div className='flex items-center my-4 justify-start gap-5'>
+            <div className='flex border-2 active:border-black border-gray-100 p-2 rounded-xl items-center my-2 justify-start gap-3'>
                 <h2 className='bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
                 <h4 className='font-medium'>A2/303, Saviour Park, Mohan Nagar, Ghaziabad</h4>
             </div>
 
-            <div className='flex items-center my-4 justify-start gap-5'>
+            <div className='flex border-2 active:border-black border-gray-100 p-2 rounded-xl items-center my-2 justify-start gap-3'>
                 <h2 className='bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
                 <h4 className='font-medium'>A2/303, Saviour Park, Mohan Nagar, Ghaziabad</h4>
             </div>
 
-            <div className='flex items-center my-4 justify-start gap-5'>
+            <div className='flex border-2 active:border-black border-gray-100 p-2 rounded-xl items-center my-2 justify-start gap-3'>
                 <h2 className='bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
                 <h4 className='font-medium'>A2/303, Saviour Park, Mohan Nagar, Ghaziabad</h4>
             </div>
 
-            <div className='flex items-center my-4 justify-start gap-5'>
+            <div className='flex border-2 active:border-black border-gray-100 p-2 rounded-xl items-center my-2 justify-start gap-3'>
                 <h2 className='bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
                 <h4 className='font-medium'>A2/303, Saviour Park, Mohan Nagar, Ghaziabad</h4>
             </div>
 
-            <div className='flex items-center my-4 justify-start gap-5'>
+            <div className='flex border-2 active:border-black border-gray-100 p-2 rounded-xl items-center my-2 justify-start gap-3'>
                 <h2 className='bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
                 <h4 className='font-medium'>A2/303, Saviour Park, Mohan Nagar, Ghaziabad</h4>
             </div>
